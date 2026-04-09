7dbd1b04f608cced2a0ad9d7a6808cf2c51a19b6
Md zuhi <mdzuhail2006@gmail.com>
2026-04-09 13:34:07 +0530

    Added Day-15

---
+++ Day-15/leetcode387.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer>  out = new HashMap<>();
+        char[] arr = s.toCharArray();
+        for(int i = 0;i<arr.length;i++){
+            char ch = arr[i];
+            out.put(ch,out.getOrDefault(ch,0) +1);   
+        }
+        for(int i = 0;i<arr.length;i++){
+            int count = out.get(arr[i]);
+            if(count==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
